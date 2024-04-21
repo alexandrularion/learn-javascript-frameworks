bd618cf3c1eea0a7a473742c364df588c63e0882
alexandrularion <larion.a@yahoo.com>
2024-04-21 10:29:14 +0300

    Updated 3_hooks_react_app from JavaScriptRO38

--- JavaScriptRO38/3_hooks_react_app/src/components/WithEffect.js
+++ JavaScriptRO38/3_hooks_react_app/src/components/WithEffect.js
@@ -1,12 +1,56 @@
 import { useState, useEffect } from "react";
 
 const WithEffect = () => {
-  const [message, setMessage] = useState("");
+  const [location, setLocation] = useState("Romania");
+  const [data, setData] = useState({
+    lat: 0,
+    lon: 0,
+    temperature: 0,
+    wind: 0,
+    humidty: 0,
+  });
+
+  const getWeatherDataFromAPI = async (location) => {
+    // we call the rapidapi weater endpoint with specific credentials
+    const response = await fetch(
+      `https://weatherapi-com.p.rapidapi.com/current.json?q=${location}`,
+      {
+        headers: {
+          "X-RapidAPI-Key":
+            "629983b172msh2195c03d7d9c635p16edd1jsna1927b61bbf0",
+          "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+        },
+      }
+    );
+
+    // We convert the response to common JSON format
+    const data = await response.json();
+
+    // We set the data state in a preffered format
+    if (data?.location && data?.current) {
+      setData({
+        lat: data.location.lat,
+        lon: data.location.lon,
+        temperature: data.current.temp_c,
+        wind: data.current.wind_kph,
+        humidty: data.current.humidity,
+      });
+    }
+  };
 
   useEffect(() => {
-    console.log("Our useEffect run once");
-    console.log(message);
-  }, [message]);
+    console.log("Our useEffect run once without any dependency");
+  }, []);
+
+  useEffect(() => {
+    // Call the weather API to get specific params about weather condition
+    getWeatherDataFromAPI(location);
+  }, [location]);
+
+  useEffect(() => {
+    // Every time the data state is changing the useEffect callback (fn) will run
+    console.log("data", data);
+  }, [data]);
 
   return (
     <div>
@@ -14,8 +58,17 @@ const WithEffect = () => {
         type="text"
         placeholder="Put location"
         name={"location"}
-        onChange={(e) => setMessage(e.target.value)}
+        value={location}
+        onChange={(event) => setLocation(event.target.value)}
       />
+      <div>
+        <h2>temperature: {data.temperature}C</h2>
+        <p>
+          lat: {data.lat}, long: {data.lon}
+        </p>
+        <p>humidity: {data.humidty}</p>
+        <p>wind: {data.wind} kph</p>
+      </div>
     </div>
   );
 };
