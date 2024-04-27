36f4fe25622f96a419116ff29d5d1bb9d39715a2
alexandrularion <larion.a@yahoo.com>
2024-04-27 10:41:52 +0300

    Added countries-react-app for JavaScriptRO38

---
+++ JavaScriptRO38/projects/countries-react-app/src/components/filters/index.jsx
@@ -0,0 +1,6 @@
+
+const Filters = () => {
+    return(<div></div>)
+}
+
+export default Filters;
---
+++ JavaScriptRO38/projects/countries-react-app/src/components/navigation/index.jsx
@@ -0,0 +1,12 @@
+import Container from "./index.styled";
+
+const Navigation = () => {
+  return (
+    <Container>
+      <h1>Where in the world?</h1>
+      <button>Light Mode</button>
+    </Container>
+  );
+};
+
+export default Navigation;
