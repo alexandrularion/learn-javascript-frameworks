71b51d67c75c67dcec66dad6e3b4f2319e67c58b
alexandrularion <larion.a@yahoo.com>
2024-07-20 11:09:12 +0300

    Updated projects for JavaScriptRemote40

--- JavaScriptRO40/projects/react_xo_game/src/App.jsx
+++ JavaScriptRO40/projects/react_xo_game/src/App.jsx
@@ -1,5 +1,12 @@
+import Heading from "./common/heading/heading";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Heading color="red">My custom heading</Heading>
+      <Heading size="sm">Size SM</Heading>
+    </>
+  );
 }
 
 export default App;
