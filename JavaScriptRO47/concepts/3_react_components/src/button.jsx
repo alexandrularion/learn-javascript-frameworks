18d5d4a3a09d68f17c839889867cc0760a23ec29
alexandrularion <larion.a@yahoo.com>
2025-01-18 15:24:57 +0200

    Updated the concepts for JavaScriptRO47

--- JavaScriptRO47/concepts/3_react_components/src/button.jsx
+++ JavaScriptRO47/concepts/3_react_components/src/button.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+
 const Button = () => {
-  return <button>Sign In</button>;
+  // Tip: useState hook always returns an array of 2 elements
+  // Tip: 1st element - getter - represents the value of the state
+  // Tip: 2nd element - setter - represents the function that can update the state
+  const [counter, setCounter] = React.useState(1); // Tip: the setter should always start with "set" prefix
+
+  let counter2 = counter; // Tip: The variables which include any state or props will automatically update
+
+  // Tip: The component (ui returned) will always update due to state or props modifications
+  return (
+    <button
+      onClick={() => {
+        setCounter(counter + 10);
+      }}
+    >
+      Increment the number: {counter}/ Counter2: {counter2}
+    </button>
+  );
 };
 
 export default Button;
