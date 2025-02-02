cec91cb3fcefe2e2a1adac7e2cf356a7cbfbe457
alexandrularion <larion.a@yahoo.com>
2025-02-02 15:18:24 +0200

    Updated projects for JavaScriptRO47

---
+++ JavaScriptRO47/projects/product-feedback-app/src/features/template.jsx
@@ -0,0 +1,7 @@
+import styles from "./template.module.css";
+
+const Template = () => {
+  return <div className={styles["container"]}>Template</div>;
+};
+
+export default Template;
---
+++ JavaScriptRO47/projects/product-feedback-app/src/pages/template.page.jsx
@@ -0,0 +1,7 @@
+import styles from "./template.module.css";
+
+const TemplatePage = () => {
+  return <main className={styles["container"]}>Template page</main>;
+};
+
+export default TemplatePage;
