d8408ecfefbe12599f97026ead5a1fdfcd7553de
alexandrularion <larion.a@yahoo.com>
2025-02-10 00:33:13 +0200

    Updated concepts for JavaScriptRO47

--- JavaScriptRO47/projects/product-feedback-app/src/features/suggestions/components/side-bar.jsx
+++ JavaScriptRO47/projects/product-feedback-app/src/features/suggestions/components/side-bar.jsx
@@ -1,12 +1,44 @@
 import styles from "./side-bar.module.css";
 import { Heading, Text } from "../../../common";
+import React from "react";
 
 const SideBar = () => {
+  const [categories, setCategories] = React.useState([]);
+
+  const getCategoriesFromApi = async () => {
+    try {
+      const response = await fetch("http://localhost:5174/api/categories");
+      const data = await response.json();
+      setCategories(data);
+    } catch (e) {
+      console.log(
+        `[API-ERROR]: Error when fetching the categories. Message: ${e.message}`
+      );
+    }
+  };
+
+  React.useEffect(() => {
+    // Tip: Will be called once (when the component is mounted)
+    // Tip: Retrieve all the categories from the backend API
+    getCategoriesFromApi();
+  }, []);
+
   return (
     <div className={styles["container"]}>
       <div className={styles["container__gradient"]}>
         <Heading size="lg">Product Feedback</Heading>
         <Text size="md">Feedback Board</Text>
+      </div>
+      <div className={styles["container__categories"]}>
+        {categories && categories.length ? (
+          categories.map((category) => (
+            <p key={category.id} style={{ color: "red" }}>
+              {category.name}
+            </p>
+          ))
+        ) : (
+          <Text>Not categories found.</Text>
+        )}
       </div>
     </div>
   );
