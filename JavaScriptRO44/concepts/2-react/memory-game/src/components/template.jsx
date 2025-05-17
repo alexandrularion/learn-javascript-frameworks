6db7b9dff1f2ee264c3b57581649d82059086b1d
alexandrularion <larion.a@yahoo.com>
2025-05-17 12:12:39 +0300

    Updated concepts for JavaScriptRO44

---
+++ JavaScriptRO44/concepts/2-react/memory-game/src/components/template.jsx
@@ -0,0 +1,11 @@
+import styles from "./template.module.css";
+
+const Template = () => {
+  return (
+    <div className={styles["container"]}>
+      <p className={styles["my-paragraph"]}>Template component</p>
+    </div>
+  );
+};
+
+export default Template;
